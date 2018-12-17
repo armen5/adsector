b5f2709be9e916661724442b0e97b9b9b7bca175
armen5 <arm.khachatryan11@gmail.com>
2018-12-17 18:27:48 +0400

    release

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="banner py-4">
+		<div class="container container-bnr">
+			<div class="row">
+				<div class="col-md-7">
+					<div class="bnr-lft">
+						<p class="p1-bnr wow slideInLeft" style="visibility: visible; animation-name: slideInLeft;">SEARCH … <span>FIND …</span><br class="for-mob"> SPIN …</p>
+						<p class="p2-bnr wow slideInLeft" style="visibility: visible; animation-name: slideInLeft;">Take the guesswork out of designing ads that grab attention. Use what’s already working on the internet to design the kind of advertisements that will get results! AdSector shows you exactly what’s working right now on the internet so you can create the most effective messages to grab the interest of potential prospects.</p>
+						<a href="/register"><p class="btn-txt btn-bnr wow zoomIn" style="visibility: visible; animation-name: zoomIn;">Try It now!</p></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+@endsection
