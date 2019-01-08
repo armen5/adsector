cb1448afce17e507f819355475c9c6b6f3f2080c
armen5 <arm.khachatryan11@gmail.com>
2019-01-08 18:46:15 +0400

    add package facebook/graph-sdk

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -10,7 +10,7 @@ use App\User;
 use PDF;
 use App\PaymentsHistory;
 use Illuminate\Validation\Rule;
-
+/*
 use FacebookAds\Api;
 use FacebookAds\Object\AdSet;
 use FacebookAds\Object\AdAccount;
@@ -18,10 +18,13 @@ use FacebookAds\Object\Fields\AdSetFields;
 use FacebookAds\Object\Fields\AdAccountFields;
 use FacebookAds\Object\Fields\CampaignFields;
 use FacebookAds\Object\Campaign;
-use LaravelFacebookAds\Clients\Facebook;
+use LaravelFacebookAds\Clients\Facebook;*/
 // use LaravelFacebookAds\Clients\Facebook;
 // use LaravelFacebookAds\Services\FacebookAdsService;
-use Edbizarro\LaravelFacebookAds\Facades\FacebookAds;
+// use Edbizarro\LaravelFacebookAds\Facades\FacebookAds;
+
+
+use Facebook\Facebook;
 
 class AppController extends Controller {
 	/**
@@ -32,9 +35,8 @@ class AppController extends Controller {
 
 	protected $facebookClient;
 
-	public function __construct(Facebook $facebookClient) {
+	public function __construct() {
 		$this->middleware('auth');
-        $this->facebookClient = $facebookClient;
 	}
 
 	/**
@@ -43,59 +45,60 @@ class AppController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function index() {
+    dd(env('ACT_ID'));
+      $appId = '232405977635045';
+      $appSecret = '499ef46003c22a8da41437f90cb1df40';
+      $person_id = '101270874301166';
+      $userAccessToken = 'EAADTXzu9mOUBAH8ZB0xyJQncMnWbjgoYmsNlI6QyKqppZCOncCc34oq49luCSuQQ37Q4NMIg1A3cl9Pn36kPoVe8yOcgnOcNXyomQOku7SktgznPvOARNjyl0k5Vqkf2sgY40MJmqHrTybA7uVu6ZCdYGFhgRcZD';
+      $access_token = 'EAADTXzu9mOUBAEar9vtBTF5uzj5cJ2CjbqAlPzW35I5uyX4AHocpBJMqx23Ns0S9E86mYW05e3DEfosFhH4wZBFqko0E1BY3qDdWscVe5WyeNkTtd3ZA97f16w25acWKmz5mXmJybHmixoQLXrBhzA2grXdjJrJwtVatohqBkRJtX4j3bfiB596xD9hzNbap5OtPIBga4tmjWH9rv9dhzA8QU4vHxGTmB0vSiqegZDZD';
+      $ACT_ID = '314812122614814';
 
-        $app_id = env('FB_ADS_APP_ID');
-        $app_secret = env('FB_ADS_APP_SECRET');
-        $access_token = "EAAIf2dIi7ikBABtt13RnREkR6Pzk4tOgc6GOEh6EiikySGep8GgNHFKrRwgOXvmWYdxTxjyZCMckmC0e6EXAlKRmQvMGEZBNyzOek8WMZBMZByZCVgZAWIYphiCLFLbY2ElB5QgUK46Re1gS6J9NjelgErVaZAE8UNZB6HBFCxI2pprJDqZAWMs65XTc4liLZC8vKDb1wWnacHfVQYZCOXwDnMUtBZCoOBfrTlUo62l21rxTsgZDZD";
-        $account_id = env('ACT_ID');
+      $fb = new Facebook([
+        'app_id' => $appId,
+        'app_secret' => $appSecret,
+        'default_graph_version' => 'v2.10',
+        'default_access_token' => $access_token, // optional
+      ]);
+      try {
+          // Returns a `Facebook\FacebookResponse` object
+          $response = $fb->get('/PROFitdev/feed',$access_token);
+        } catch(\Facebook\Exceptions\FacebookResponseException $e) {
+          echo 'Graph returned an error: ' . $e->getMessage();
+          exit;
+        } catch(\Facebook\Exceptions\FacebookSDKException $e) {
+          echo 'Facebook SDK returned an error: ' . $e->getMessage();
+          exit;
+        }
+        $graphNode = $response->getGraphNode();
+        dd($graphNode);
 
-        Api::init($app_id, $app_secret, $access_token);
+      // Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+        $helper = $fb->getPageTabHelper();
+        dd($helper);
+        // $helper = $fb->getJavaScriptHelper();
+        // $helper = $fb->getCanvasHelper();
+        // $helper = $fb->getPageTabHelper();
 
-        $client = $this->facebookClient;
-        // dd($client->account($account_id)->ads());
+      try {
+        // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+        // If you provided a 'default_access_token', the '{access-token}' is optional.
+        // $response = $fb->get("/$person_id/", $access_token);
+        $accessToken = $helper->getAccessToken();
+      } catch(\Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+      } catch(\Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+      }
 
-        
+      // $me = $response->getGraphUser();
+      dd($accessToken);
+      exit();
+		  return view('dashboard');
 
-        $account = new AdAccount($account_id);
-        // dd($account->read());
-
-        FacebookAds::init($access_token);
-        $ads = FacebookAds::adAccounts()->all()->map(function ($adAccount) {
-        return $adAccount->ads(
-              [
-                  'name',
-                  'account_id',
-                  'account_status',
-                  'balance',
-                  'campaign',
-                  'campaign_id',
-                  'status'
-              ]
-          );
-        });
-        // dd($ads);
-
-
-		return view('dashboard');
-        $account = new AdAccount($account_id);
-
-        $fields = array(
-            CampaignFields::ID,
-            CampaignFields::NAME,
-            CampaignFields::START_TIME,
-            CampaignFields::STOP_TIME,
-            CampaignFields::SPEND_CAP,
-            'effective_status'
-        );
-
-        $params = array(
-            'effective_status' => array(
-                Campaign::STATUS_ACTIVE
-            ),
-        );
-
-        $campaignSets = $account->getCampaigns($fields, $params);
-        dd($campaignSets);
 	}
 
 	public function accountMemberView() {
